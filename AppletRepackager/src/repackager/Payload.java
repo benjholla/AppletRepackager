b77cea5154d0b38206b3fd78bac343df19e2f397
benjholla <benjholla@gmail.com>
2014-02-09 23:58:31 -0600

    some comments

--- AppletRepackager/src/repackager/Payload.java
+++ AppletRepackager/src/repackager/Payload.java
@@ -2,6 +2,18 @@ package repackager;
 
 import java.awt.Graphics;
 
+/**
+Except from http://www.msci.memphis.edu/~simmonsj/c4302/notes/staging/chap3.html
+
+To run an applet, the browser creates an instance of its class. The instance, or object, 
+occupies a chunk of memory and holds any information pertaining to the applet. After constructing 
+the instance, the browser calls its init() method, then its start() method, then its paint(g) method.
+...
+When the applet is finally removed from the browser, say at browser shutdown, the browser calls its 
+destroy() method for final cleanup.
+
+This interface specifies pre and post payload insertion points.
+*/
 public interface Payload {
 
 	public void preInitPayload();
