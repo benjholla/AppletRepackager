fc0478b848a5a8a9da78eab31e3300228a9924df
benjholla <benjholla@gmail.com>
2014-04-05 11:20:36 -0500

    Added a GUI shell (no logic yet), added a shell for a wifi survey payloda

---
+++ AppletRepackager/src/repackager/payloads/WifiSurveyPayload.java
@@ -0,0 +1,59 @@
+package repackager.payloads;
+
+import java.awt.Graphics;
+
+import repackager.Payload;
+
+public class WifiSurveyPayload implements Payload {
+
+	@Override
+	public void preInitPayload() {
+		// TODO: Implement
+	}
+
+	@Override
+	public void postInitPayload() {
+		
+	}
+
+	@Override
+	public void preStartPayload() {
+		
+	}
+
+	@Override
+	public void postStartPayload() {
+		
+	}
+
+	@Override
+	public void prePaintPayload(Graphics g) {
+		
+	}
+
+	@Override
+	public void postPaintPayload(Graphics g) {
+		
+	}
+
+	@Override
+	public void preStopPayload() {
+			
+	}
+
+	@Override
+	public void postStopPayload() {
+		
+	}
+
+	@Override
+	public void preDestroyPayload() {
+		
+	}
+
+	@Override
+	public void postDestroyPayload() {
+		
+	}
+
+}
